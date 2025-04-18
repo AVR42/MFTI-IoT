c3f0a1d4cb4d8a64c216fe5a7ff7d80496c50e8a
AVR42 <155975032+AVR42@users.noreply.github.com>
2025-04-18 18:49:43 +0300

    Update temp_functions.h

--- HW13/temp_functions.h
+++ HW13/temp_functions.h
@@ -1,19 +1,12 @@
 #ifndef TEMP_FUNCTIONS_H
 #define TEMP_FUNCTIONS_H
 
-#define MAX_READINGS 100000
-
 typedef struct {
-    int year;
-    int month;
-    int day;
-    int hour;
-    int minute;
-    int temperature;
+    int year, month, day, hour, minute, temperature;
 } Reading;
 
 void print_help();
-int read_csv(const char *filename, Reading *readings);
+int read_csv_file(const char *filename, Reading **readings);
 void print_month_stats(const Reading *readings, int count, int month);
 void print_all_months_stats(const Reading *readings, int count);
 void print_year_stats(const Reading *readings, int count);
