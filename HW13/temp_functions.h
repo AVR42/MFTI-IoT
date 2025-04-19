1b70aa63cf090efc115722538f138bb5798e19cc
AVR42 <155975032+AVR42@users.noreply.github.com>
2025-04-19 10:48:33 +0300

    Update temp_functions.h

--- HW13/temp_functions.h
+++ HW13/temp_functions.h
@@ -1,14 +1,14 @@
-#ifndef TEMP_FUNCTIONS_H
-#define TEMP_FUNCTIONS_H
+#ifndef TEMP_FUNCTIONS_H //include guard
+#define TEMP_FUNCTIONS_H //
 
 typedef struct {
     int year, month, day, hour, minute, temperature;
-} Reading;
+} Reading; // определение структуры Reading 
 
-void print_help();
-int read_csv_file(const char *filename, Reading **readings);
-void print_month_stats(const Reading *readings, int count, int month);
-void print_all_months_stats(const Reading *readings, int count);
-void print_year_stats(const Reading *readings, int count);
+void print_help(); // вывести справку
+int read_csv_file(const char *filename, Reading **readings); // чтение файла и запись
+void print_month_stats(const Reading *readings, int count, int month); //вывести статистику по месяцу
+void print_all_months_stats(const Reading *readings, int count); // вывести статистику по всем 12 месяцам каждый
+void print_year_stats(const Reading *readings, int count); // вывести статистику по году
 
 #endif
