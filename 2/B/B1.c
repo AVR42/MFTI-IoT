42f98d1dbd8da328346c15e7075b6b7a115a5bdc
AVR42 <155975032+AVR42@users.noreply.github.com>
2026-04-24 21:03:59 +0300

    Update B1.c

--- 2/B/B1.c
+++ 2/B/B1.c
@@ -1,1 +1,51 @@
+/*
+ * B1
+ * 
+ * Copyright 2026 litki <litki@LVM>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <stddef.h>
+
+/* 
+Структура из условия:
+typedef struct list {
+    void *address;
+    size_t size;
+    char comment[64];
+    struct list *next;
+} list;
+*/
+
+size_t totalMemoryUsage(list *head) {
+    size_t total = 0;
+    list *current = head;
+
+    // Проходим по списку, пока не встретим NULL
+    while (current != NULL) {
+        // Суммируем размер текущего блока
+        total += current->size;
+        // Переходим к следующему элементу
+        current = current->next;
+    }
+
+    return total;
+}
 
